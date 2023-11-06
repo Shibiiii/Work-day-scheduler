108a545bd7db18e5912fb29b4020a9ebad764030
Shibiiii <purplejohn13@gmail.com>
2023-11-05 17:38:40 -0800

    created variables for each hour

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,15 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var hour9 = $('#hour-9');
+  var hour10 = $('#hour-10');
+  var hour11 = $('#hour-11');
+  var hour12 = $('#hour-12');
+  var hour1 = $('#hour-1');
+  var hour2 = $('#hour-2');
+  var hour3 = $('#hour-3');
+  var hour4 = $('#hour-4');
+  var hour5 = $('#hour-5');
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
