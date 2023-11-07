70dcc2424bf5feba20ec8a4a3873dc10d6cd752a
Shibiiii <purplejohn13@gmail.com>
2023-11-06 21:02:28 -0800

    if else statments written for hour changing

--- Develop/script.js
+++ Develop/script.js
@@ -50,10 +50,76 @@ $(function () {
   var futureHour = dayjs().isAfter().hour()
 
   if ($('#hour-9') === currentHour) {
-    $('#hour-9').replaceClass('row time-block present');
+    $('#hour-9').replaceClass('row time-block present')
   } else if ($('#hour-9') === pastHour) {
-    
-  }
+    $('#hour-9').replaceClass('row time-block past')
+  } else ($('#hour-9') === futureHour) ;{
+    $('#hour-9').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-10') === currentHour) {
+    $('#hour-10').replaceClass('row time-block present')
+  } else if ($('#hour-10') === pastHour) {
+    $('#hour-10').replaceClass('row time-block past')
+  } else ($('#hour-10') === futureHour) ;{
+    $('#hour-10').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-11') === currentHour) {
+    $('#hour-11').replaceClass('row time-block present')
+  } else if ($('#hour-11') === pastHour) {
+    $('#hour-11').replaceClass('row time-block past')
+  } else ($('#hour-11') === futureHour) ;{
+    $('#hour-11').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-12') === currentHour) {
+    $('#hour-12').replaceClass('row time-block present')
+  } else if ($('#hour-12') === pastHour) {
+    $('#hour-11').replaceClass('row time-block past')
+  } else ($('#hour-12') === futureHour) ;{
+    $('#hour-12').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-1') === currentHour) {
+    $('#hour-1').replaceClass('row time-block present')
+  } else if ($('#hour-1') === pastHour) {
+    $('#hour-1').replaceClass('row time-block past')
+  } else ($('#hour-1') === futureHour) ;{
+    $('#hour-1').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-2') === currentHour) {
+    $('#hour-2').replaceClass('row time-block present')
+  } else if ($('#hour-2') === pastHour) {
+    $('#hour-2').replaceClass('row time-block past')
+  } else ($('#hour-2') === futureHour) ;{
+    $('#hour-2').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-3') === currentHour) {
+    $('#hour-3').replaceClass('row time-block present')
+  } else if ($('#hour-3') === pastHour) {
+    $('#hour-3').replaceClass('row time-block past')
+  } else ($('#hour-3') === futureHour) ;{
+    $('#hour-3').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-4') === currentHour) {
+    $('#hour-4').replaceClass('row time-block present')
+  } else if ($('#hour-4') === pastHour) {
+    $('#hour-4').replaceClass('row time-block past')
+  } else ($('#hour-4') === futureHour) ;{
+    $('#hour-4').replaceClass('row time-block future')
+  };
+
+  if ($('#hour-5') === currentHour) {
+    $('#hour-5').replaceClass('row time-block present')
+  } else if ($('#hour-5') === pastHour) {
+    $('#hour-5').replaceClass('row time-block past')
+  } else ($('#hour-5') === futureHour) ;{
+    $('#hour-5').replaceClass('row time-block future')
+  };
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
