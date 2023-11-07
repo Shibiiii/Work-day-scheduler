08994d12585e8496eaeb2f10ca67dd857ac2ae30
Shibiiii <purplejohn13@gmail.com>
2023-11-06 18:28:12 -0800

    moved date code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var dayEl = $('#currentDay');
+var today = dayjs();
+var formattedDate = today.format('MM-DD-YYYY');
+
+dayEl.text(formattedDate)
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -28,15 +35,10 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-
-  var dayEl = $('#currentDay');
-  var today = dayjs();
-  var formattedDate = today.format('MM-DD-YYYY');
-
-  dayEl.text(formattedDate)
 });
